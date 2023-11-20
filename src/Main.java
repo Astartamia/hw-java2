747d579ff02e32aaacb1097e6400b458eb7221cc
Mila Ekshibarova <Sokur.luda13@gmail.com>
2023-11-20 17:34:02 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 9213;
+
+        System.out.println("Ваши бонусные мили:" + (ticketPrice / 20));
+    }
+}
